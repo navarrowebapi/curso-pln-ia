6291fccb2386eda2d37079b16d0486b33a383b51
Navarro <navarro.fabio@gmail.com>
2025-06-10 19:32:39 -0300

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react( )],
+  base: '/curso-pln-ia/', // Certifique-se de que este é o nome do seu repositório no GitHub
+})
